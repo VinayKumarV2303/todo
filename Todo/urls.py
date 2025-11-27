fa5a2fed59ba9534d0de09ddfe5ecdbd2448b884
Vinay Kumar <vinaykumarv2303@gmail.com>
2025-11-27 19:45:49 +0530

    Wire board homepage and fix templates path

--- Todo/urls.py
+++ Todo/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from board.views import board_view  # your task board view
 from rest_framework.routers import DefaultRouter
-from board.views import TaskViewSet  # or wherever TaskViewSet is
+
+from board.views import board_view, TaskViewSet
 
 router = DefaultRouter()
 router.register(r"tasks", TaskViewSet, basename="task")
@@ -10,5 +10,5 @@ router.register(r"tasks", TaskViewSet, basename="task")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-    path("", board_view, name="board"),  # 👈 this must exist
+    path("", board_view, name="board"),  # 👈 homepage
 ]
--- board/views.py
+++ board/views.py
@@ -1,5 +1,21 @@
-# board/views.py
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+
 
 def board_view(request):
-    return render(request, "board.html")  # this will look for templates/board.html
+    # renders templates/board.html
+    return render(request, "board.html")
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        developer = self.request.query_params.get("developer")
+        if developer:
+            qs = qs.filter(developer=developer)
+        return qs
